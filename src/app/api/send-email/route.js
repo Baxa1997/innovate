5d93572640cf9fff258d91a101ec2a89f3078617
Moxrbe <one393752@gmail.com>
2024-02-16 09:04:50 +0500

    changed gmail

--- src/app/api/send-email/route.js
+++ src/app/api/send-email/route.js
@@ -32,21 +32,20 @@ export async function POST(req, res) {
   const transporter  = nodemailer.createTransport({
     service: "gmail",
     auth: {
-      user: "one393752@gmail.com",
-      pass: "yqemunfzxjlhifgq",
+      user: "ZeroMaxeld7@gmail.com",
+      pass: "fpkxncrhawzmufgk",
     },
   })
-
    const mailOptions = {
-    from: "one393752@gmail.com",
+    from: "ZeroMaxeld7@gmail.com",
     to: data.email,
     subject: "ZeroMAX ELD"
   };
 
- await transporter.sendMail({
+  await transporter.sendMail({
         ...mailOptions,
         ...generateEmailContent({data: data.name}),
-   });
+  });
 
   return NextResponse.json({message: "this is post"})
   };
--- src/lib/api.js
+++ src/lib/api.js
@@ -4,6 +4,9 @@ export const sendContactForm = async (data) =>
     body: JSON.stringify(data),
     headers: { "Content-Type": "application/json", Accept: "application/json" },
   }).then((res) => {
+    console.log("res", res)
     if (!res.ok) throw new Error("Failed to send message");
     return res.json();
-  });
+  }).catch((err) => {
+    console.log("err", err)
+  })
